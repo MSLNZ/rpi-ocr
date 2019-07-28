cf53fc03892650ec3b5d57df624478940a1762ba
jborbely <joe.borbely@gmail.com>
2019-07-29 10:43:07 +1200

    add libjasper-dev libqtgui4 libqt4-test; remove wget calls

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install prerequisites
-sudo apt install python3-venv libffi-dev libssl-dev build-essential libimlib2 libimlib2-dev automake libtool libleptonica-dev make pkg-config libicu-dev libpango1.0-dev libcairo2-dev libatlas-base-dev -y
+sudo apt install python3-venv libffi-dev libssl-dev build-essential libimlib2 libimlib2-dev automake libtool libleptonica-dev make pkg-config libicu-dev libpango1.0-dev libcairo2-dev libatlas-base-dev libjasper-dev libqtgui4 libqt4-test -y
 
 # build tesseract with training tools
 git clone https://github.com/tesseract-ocr/tesseract.git
@@ -28,21 +28,6 @@ cd ssocr/
 sudo make install
 cd ..
 
-# test tesseract and ssocr installation
-wget https://files.realpython.com/media/sample2.36f8074c5273.jpg
-wget https://www.unix-ag.uni-kl.de/~auerswal/ssocr/six_digits.png
-echo Testing tesseract installation... you should see 619121
-tesseract sample2.36f8074c5273.jpg out && more out.txt
-echo Testing ssocr installation... you should see 431432
-ssocr -T six_digits.png
-
-# delete the files that are no longer required
-rm -rf tesseract/
-rm -rf ssocr/
-rm six_digits.png
-rm sample2.36f8074c5273.jpg
-rm out.txt
-
 # create a python virtual environment and install rpi-ocr
 python3 -m venv ocrenv
 source ocrenv/bin/activate
@@ -50,3 +35,13 @@ pip install --upgrade pip
 pip install --upgrade setuptools
 pip install https://github.com/MSLNZ/rpi-ocr/archive/master.tar.gz
 deactivate
+
+# test tesseract and ssocr installation
+echo Testing tesseract installation... you should see 619121
+tesseract rpi-ocr/tests/images/tesseract_numbers.jpg stdout
+echo Testing ssocr installation... you should see 431432
+ssocr -T rpi-ocr/tests/images/six_digits.png
+
+# cleanup
+rm -rf tesseract/
+rm -rf ssocr/
