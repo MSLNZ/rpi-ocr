ea1198f3cdd7968fb13a7f67177d3cd7ecb64208
jborbely <joe.borbely@gmail.com>
2020-05-09 21:29:09 +1200

    use pip to install wheel, pytest-cov, rpi-ocr and make pyside2-deb-tmp folder

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -37,36 +37,36 @@ cd ssocr/
 sudo make install
 cd ~
 
+# create virtual environment
+python3 -m venv $ENV_NAME
+source $ENV_NAME/bin/activate
+pip install --upgrade pip
+pip install --upgrade setuptools wheel
+
+# install PySide2 in the virtual environment
+mkdir pyside2-deb-tmp
+cd pyside2-deb-tmp
+apt download python3-pyside2.qt3dcore python3-pyside2.qt3dinput python3-pyside2.qt3dlogic python3-pyside2.qt3drender python3-pyside2.qtcharts python3-pyside2.qtconcurrent python3-pyside2.qtcore python3-pyside2.qtgui python3-pyside2.qthelp python3-pyside2.qtlocation python3-pyside2.qtmultimedia python3-pyside2.qtmultimediawidgets python3-pyside2.qtnetwork python3-pyside2.qtopengl python3-pyside2.qtpositioning python3-pyside2.qtprintsupport python3-pyside2.qtqml python3-pyside2.qtquick python3-pyside2.qtquickwidgets python3-pyside2.qtscript python3-pyside2.qtscripttools python3-pyside2.qtsensors python3-pyside2.qtsql python3-pyside2.qtsvg python3-pyside2.qttest python3-pyside2.qttexttospeech python3-pyside2.qtuitools python3-pyside2.qtwebchannel python3-pyside2.qtwebsockets python3-pyside2.qtwidgets python3-pyside2.qtx11extras python3-pyside2.qtxml python3-pyside2.qtxmlpatterns python3-pyside2uic
+for f in *.deb; do dpkg -x $f ./pyside2; done;
+cp -r ./pyside2/usr/lib/python3/dist-packages/* ~/$ENV_NAME/lib/python*/site-packages/
+cd ~
+
 # check tesseract and ssocr installation
 echo Testing tesseract installation... you should see 619121
 tesseract rpi-ocr/tests/images/tesseract_numbers.jpg stdout
 echo Testing ssocr installation... you should see 431432
 ssocr -T rpi-ocr/tests/images/six_digits.png
 
-# create virtual environment
-python3 -m venv $ENV_NAME
-source $ENV_NAME/bin/activate
-pip install --upgrade pip
-pip install --upgrade setuptools
-
-# install PySide2 in the virtual environment
-apt download python3-pyside2.qt3dcore python3-pyside2.qt3dinput python3-pyside2.qt3dlogic python3-pyside2.qt3drender python3-pyside2.qtcharts python3-pyside2.qtconcurrent python3-pyside2.qtcore python3-pyside2.qtgui python3-pyside2.qthelp python3-pyside2.qtlocation python3-pyside2.qtmultimedia python3-pyside2.qtmultimediawidgets python3-pyside2.qtnetwork python3-pyside2.qtopengl python3-pyside2.qtpositioning python3-pyside2.qtprintsupport python3-pyside2.qtqml python3-pyside2.qtquick python3-pyside2.qtquickwidgets python3-pyside2.qtscript python3-pyside2.qtscripttools python3-pyside2.qtsensors python3-pyside2.qtsql python3-pyside2.qtsvg python3-pyside2.qttest python3-pyside2.qttexttospeech python3-pyside2.qtuitools python3-pyside2.qtwebchannel python3-pyside2.qtwebsockets python3-pyside2.qtwidgets python3-pyside2.qtx11extras python3-pyside2.qtxml python3-pyside2.qtxmlpatterns python3-pyside2uic
-for f in *.deb; do dpkg -x $f ./pyside2-tmp; done;
-cp -r ./pyside2-tmp/usr/lib/python3/dist-packages/* ./$ENV_NAME/lib/python*/site-packages/
-
-# install rpi-ocr
+# run the rpi-ocr tests
+pip install pytest pytest-cov
 cd rpi-ocr
-python setup.py install
-
-# run the Python tests
-pip install pytest
 pytest
 
-cd ~
-deactivate
+# install rpi-ocr
+pip install .
 
 # cleanup
+cd ~
 rm -rf tesseract/
 rm -rf ssocr/
-rm -rf pyside2-tmp/
-rm *.deb
+rm -rf pyside2-deb-tmp/
