b63f0ade6d6c99c7844158bdf56e6f4a86643510
jborbely <joe.borbely@gmail.com>
2020-05-20 09:17:44 +1200

    pytesseract is now automatically installed; add debug logging to functions

--- ocr/tesseract.py
+++ ocr/tesseract.py
@@ -3,10 +3,7 @@ The `Tesseract <https://github.com/tesseract-ocr/tesseract>`_ algorithm.
 """
 import subprocess
 
-try:
-    import pytesseract
-except ImportError:
-    pytesseract = None
+import pytesseract
 
 from .utils import (
     to_cv2,
@@ -24,7 +21,6 @@ def set_tesseract_path(path):
         The full path to the ``tesseract`` executable or a top-level
         directory that contains the executable.
     """
-    _check_pytessseract_installed()
     exe = get_executable_path(path, 'tesseract')
     logger.debug('set tesseract executable to {!r}'.format(exe))
     pytesseract.pytesseract.tesseract_cmd = exe
@@ -38,8 +34,9 @@ def version():
     :class:`str`
         The version number (and possibly copyright information).
     """
-    _check_pytessseract_installed()
-    return pytesseract.get_tesseract_version()
+    ver = str(pytesseract.get_tesseract_version())
+    logger.debug('tesseract version: ' + ver)
+    return ver
 
 
 def languages():
@@ -50,10 +47,11 @@ def languages():
     :class:`list` of :class:`str`
         The languages.
     """
-    _check_pytessseract_installed()
     cmd = [pytesseract.pytesseract.tesseract_cmd, '--list-langs']
     out = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-    return out.decode('utf-8').splitlines()[1:]
+    langs = out.decode('utf-8').splitlines()[1:]
+    logger.debug('tesseract languages: ' + ', '.join(langs))
+    return langs
 
 
 def tesseract(image, *, language='eng', psm=7, oem=3, whitelist='0123456789+-.', timeout=0, nice=0, config=''):
@@ -94,7 +92,7 @@ def tesseract(image, *, language='eng', psm=7, oem=3, whitelist='0123456789+-.',
         The character set that the result must be in (equivalent to ``-c tessedit_char_whitelist``).
         Set to :data:`None` or an empty string to not use a whitelist.
     timeout : :class:`float`, optional
-        The number of seconds to wait for the result before raising a :exc:`RuntimeError`.
+        The maximum number of seconds to wait for the result before raising a :exc:`RuntimeError`.
     nice : :class:`int`, optional
         Modifies the processor priority for the Tesseract run. Not supported on Windows.
     config : :class:`str`, optional
@@ -105,7 +103,6 @@ def tesseract(image, *, language='eng', psm=7, oem=3, whitelist='0123456789+-.',
     :class:`str`
         The OCR text.
     """
-    _check_pytessseract_installed()
     cfg = '--psm {} --oem {}'.format(psm, oem)
     if whitelist:
         cfg += ' -c tessedit_char_whitelist=' + whitelist
@@ -113,10 +110,3 @@ def tesseract(image, *, language='eng', psm=7, oem=3, whitelist='0123456789+-.',
         cfg += ' ' + config
     logger.debug('tesseract params: language={!r} config={!r} nice={} timeout={}'.format(language, cfg, nice, timeout))
     return pytesseract.image_to_string(to_cv2(image), lang=language, config=cfg, nice=nice, timeout=timeout)
-
-
-def _check_pytessseract_installed():
-    if pytesseract is None:
-        # this is the same error message that pytesseract.TesseractNotFoundError would display
-        # Raise FileNotFoundError since this is what ssocr raises if it is not available
-        raise FileNotFoundError("tesseract is not installed or it's not in your path")
