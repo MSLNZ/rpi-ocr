4a593348a80517a18c77e2ffc092d7dfcd172db4
jborbely <joe.borbely@gmail.com>
2022-08-06 15:25:03 +1200

    update copyright year

--- ocr/__init__.py
+++ ocr/__init__.py
@@ -25,7 +25,7 @@ from .cameras import (
 from .services import kill_ocr_service
 
 __author__ = 'Measurement Standards Laboratory of New Zealand'
-__copyright__ = '\xa9 2020 - 2021, ' + __author__
+__copyright__ = '\xa9 2020 - 2022, ' + __author__
 __version__ = '0.1.0.dev0'
 
 _v = re.search(r'(\d+)\.(\d+)\.(\d+)[.-]?(.*)', __version__).groups()
